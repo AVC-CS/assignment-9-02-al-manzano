fca277c227ee77614be0d4f0c4859f1fc439c3c6
Alex Manzano <amanzano5@students.avc.edu>
2024-02-27 08:07:42 -0800

    code

--- main.hpp
+++ main.hpp
@@ -8,8 +8,8 @@ const int N = 5;
 struct Student
 {
     int id;
-    string name;
-    string major;
+    char name[20];
+    char major[20];
     Student *next;
 };
 
@@ -18,13 +18,40 @@ void printStudent(Student *head);
 
 Student *makeStudent()
 {
-    /*******************************
-     * Code your program here
-     *******************************/
+  Student *head, *tmp, *prev;
+  char tstr[10];
+  ifstream ifs;
+  ifs.open("students.txt");
+  if (!ifs)
+  {
+    cerr << "Error: file open error" << endl;
+    exit(0);
+  }
+  for(int i=0;i<N;i++)
+    {
+      tmp = (Student *)malloc(sizeof(Student)); 
+      ifs.getline(tstr, 20, ' ');
+      tmp->id = atoi(tstr);
+      ifs >> tmp->name;
+      ifs >> tmp->major;
+      tmp->next = NULL;
+      if (i==0)
+        head = tmp;  
+      else
+        prev->next = tmp; 
+      prev = tmp;
+    }
+  return head;
 }
 void printStudent(Student *head)
 {
-    /*******************************
-     * Code your program here
-     *******************************/
+  Student *tmp;
+  tmp = head;
+  while(tmp != NULL)
+    {
+        cout << tmp->id << "\t";
+        cout << tmp->name << "\t";
+        cout << tmp->major << "\n";
+        tmp = tmp->next;
+    }
 }
